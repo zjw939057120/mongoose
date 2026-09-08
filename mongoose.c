bad8b0d243f9dce4ed94a4bdde96abc06df86ce7
zjw939057120 <zjw939057120@outlook.com>
2026-09-08 10:32:10 +0800

    渲染页面

--- mongoose.c
+++ mongoose.c
@@ -4164,7 +4164,14 @@ void mg_http_serve_dir(struct mg_connection *c, struct mg_http_message *hm,
   } else if (flags && sp != NULL && mg_match(mg_str(path), mg_str(sp), NULL)) {
     mg_http_serve_ssi(c, opts->root_dir, path);
   } else {
-    mg_http_serve_file(c, hm, path, opts);
+    const char *html_suffix = ".html";
+    size_t suffix_len = strlen(html_suffix);
+    size_t path_len = strlen(path);
+    if (path_len >= suffix_len && memcmp(path + path_len - suffix_len, html_suffix, suffix_len) == 0) {
+      mg_http_serve_file(c, hm, "web_root/page.html", opts);
+    }else{
+      mg_http_serve_file(c, hm, path, opts);
+    }
   }
 }
 
